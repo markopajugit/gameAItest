5b52c95b3a865dee4076f72e98f244970bac4300
Marko Paju <markopaju92@gmail.com>
2024-11-18 22:51:13 +0200

    ai redid some things

--- public/game.js
+++ public/game.js
@@ -1,4 +1,6 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/0.170.0/three.module.js';
+import { io } from 'socket.io-client';
+
 const socket = io();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
